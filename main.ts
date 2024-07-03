2cb997c282119894194763cf64c7bb8dd1f4467c
iraj2468 <irajrashid2468@gmail.com>
2024-07-03 08:00:51 +0100

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,37 @@
+import inquirer from "inquirer"
+
+let answers = await inquirer.prompt([
+    {
+        message:"Enter first number",
+        type:"number",
+        name: "firstNumber"
+    },
+    {
+        message:"Enter your second Number",
+        type:"number",
+        name:"secondNumber"
+    },
+    {
+        message:"please select one operator to perform operations",
+        type:"list",
+        name:"operator",
+        choices:["Addition","Subtraction","Multiplication","Division"]
+
+    }
+])
+
+if(answers.operator === "Addition"){
+       console.log(answers.firstNumber + answers.secondNumber)
+}
+else if(answers.operator === "Subtraction"){
+    console.log(answers.firstNumber - answers.secondNumber)
+}
+else if(answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+}
+else if(answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else{
+    console.log("Invalid Input");
+}
